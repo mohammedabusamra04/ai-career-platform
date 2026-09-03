277c77936463126e65bd327e0f4741d5fe0dc82a
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-09-03 20:59:13 +0300

    feat(jobs): integrate duplicate filtering into collection pipeline (#8)

--- src/modules/jobs/job.collection.service.ts
+++ src/modules/jobs/job.collection.service.ts
@@ -1,15 +1,29 @@
-import { Job, JobSearchQuery } from './job.types.js';
+import type { Job, JobSearchQuery } from './job.types.js';
 import { JobSourceManager } from './sources/job-source.manager.js';
 import { validateJob } from './job.validator.js';
 import ms from 'ms';
 
+interface JobDeduplicator {
+  deduplicate(jobs: Job[]): Promise<{
+    uniqueJobs: Job[];
+    duplicateJobs: Job[];
+  }>;
+}
+
 export class JobCollectionService {
-  constructor(private readonly sourceManager: JobSourceManager) {}
+  constructor(
+    private readonly sourceManager: JobSourceManager,
+    private readonly deduplicationService: JobDeduplicator,
+  ) {}
 
   async collectJobs(query: JobSearchQuery): Promise<Job[]> {
     const jobs = await this.sourceManager.fetchJobs(query);
 
-    return jobs.filter(validateJob).filter((job) => this.isRecentJob(job));
+    const validRecentJobs = jobs.filter(validateJob).filter((job) => this.isRecentJob(job));
+
+    const result = await this.deduplicationService.deduplicate(validRecentJobs);
+
+    return result.uniqueJobs;
   }
 
   private isRecentJob(job: Job): boolean {
