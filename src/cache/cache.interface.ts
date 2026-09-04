6ed029e942f3815c049ce29411fe0ce36a829590
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-09-04 21:41:03 +0300

    feat(cache): extend cache interface with atomic operation (#10)

--- src/cache/cache.interface.ts
+++ src/cache/cache.interface.ts
@@ -1,6 +1,8 @@
 export interface Cache {
   set<T>(key: string, value: T, ttl?: number): Promise<void>;
 
+  setIfNotExists<T>(key: string, value: T, ttl?: number): Promise<boolean>;
+
   get<T>(key: string): Promise<T | null>;
 
   delete(key: string): Promise<void>;
